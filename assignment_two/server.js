760fc44190429536cbc65513a72c943d514ad9f4
Parham <586318@bah.com>
2018-08-01 15:22:01 -0400

    Part 2 complete

--- assignment_two/server.js
+++ assignment_two/server.js
@@ -14,19 +14,17 @@ var bodyParser = require('body-parser');
 
 
 var con = mysql.createConnection({
-    host: "127.0.0.1",
+    host: "localhost",
     user: "root", 
     password: "",
     port: 3306,
+    database: "people"
   });
 
   con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE mydb", function (err, result) {
-      if (err) throw err;
-      console.log("Database created");
-    });
+    
+    else console.log("Connected!");
   });
 
 app.use('/api', router);
@@ -43,14 +41,30 @@ app.get('/', function (req, res){
     res.sendfile(__dirname + '/html/index.html');
 });
 
-//PART II: Create a web app that allows you to change the 5 names in the database dynamically via a rest call
+//PART II: connect to any database of your choosing, and be able to grab all animals in the database or an individual animal given an id
 app.get('/\\d?', function(req, res, next){
-    console.log(req.path[1]);
-    res.sendfile(__dirname + "/html/practice.html");
+    
+    con.query("SELECT * FROM zoo WHERE id = ('"+ req.path[1] +"')",
+    function(err, result, fields){
+      if(err) 
+        console.log("Error: ", err);
+      else{
+      res.writeHead(200, {"Content-Type" : "text/plain"});
+      res.write("[\n{\n \"id\": \""+ [result[0].id] + "\",\n"
+                +" \"name\": \""+[result[0].name] + "\",\n"
+                +" \"description\": \"" + [result[0].description]+"\" \n}\n]"
+                );
+      res.end();
+      }
+    });
+});
+
+app.get('/db_pull', function(req, res, next){
+  
 });
 
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log("server started");
+console.log("server started on: ", port);
